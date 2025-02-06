6d1875dbef540521beb470518f035b30b13456bb
HP <HP@DESKTOP-068QHFA>
2025-02-06 12:46:46 +0100

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using QRCoder;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace QR_Code_Todorov
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+    
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GenerateAndDisplayQRCode();
+        }
+
+        private void GenerateAndDisplayQRCode()
+        {
+            // URL за генерирање на QR-код
+            string url = "https://www.wagner-hoscher.com/";
+
+            // Генерирај QR-код
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+                using (QRCode qrCode = new QRCode(qrCodeData))
+                {
+                    Bitmap qrCodeImage = qrCode.GetGraphic(10);
+
+                    // Прикажи го QR-кодот во Image контролата
+                    QRCodeImage.Source = ConvertBitmapToBitmapImage(qrCodeImage);
+                }
+            }
+        }
+
+        private BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+
+    }
+}
+
+
+
